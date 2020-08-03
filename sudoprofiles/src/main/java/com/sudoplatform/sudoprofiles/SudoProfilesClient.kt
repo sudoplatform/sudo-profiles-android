cd1bea80f19a04d7b07fc458cd36a664884a5efa
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2020-08-03 06:03:34 +0000

    Release 3.0.6

--- sudoprofiles/src/main/java/com/sudoplatform/sudoprofiles/SudoProfilesClient.kt
+++ sudoprofiles/src/main/java/com/sudoplatform/sudoprofiles/SudoProfilesClient.kt
@@ -544,7 +544,7 @@ class DefaultSudoProfilesClient constructor(
         private const val CONFIG_BUCKET = "bucket"
     }
 
-    override val version: String = "3.0.5"
+    override val version: String = "3.0.6"
 
     /**
      * GraphQL client used for calling Sudo service API.
